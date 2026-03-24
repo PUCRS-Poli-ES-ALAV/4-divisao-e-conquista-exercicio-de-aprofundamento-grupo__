a54a6811cb5d09a670f2c28798291b6395d3c900
Vinicius Alencar <vinicius.alencar@edu.pucrs.br>
2026-03-23 21:48:20 -0300

    Exercicio 5

--- src/main/java/br/pucrs/Ex04.java
+++ src/main/java/br/pucrs/Ex04.java
@@ -24,6 +24,11 @@ ELSE
     RETURN 2^(2m)*e + 2^m*(g + h) + f.
 
 Ajuste a assinatura da sua implementação para receber tipo inteiros long (em java).
+
+---------------------------------------------------------------------------------------
+
+Monte uma tabela com os resultados das execuções acima. As linhas da tabela são os algoritmos implementados, as colunas o tamanho dos vetores usados para testar e contabilizar.
+
 */
 
 package br.pucrs;
@@ -56,18 +61,27 @@ public class Ex04 {
         return (long) Math.pow(2, 2*m) * e + mathpow2m * (g + h) + f;
     }
     
-    public static void main(String[] args) {
-        
-        long x = 4;
-        long y = 6;
-        int n = 64;
+    public static void Ex05(long x, long y, int n){
+        iteracoes = 0;
         
         long tempoInicial = System.nanoTime();
         
-        System.out.println(MULTIPLY(x, y, n));
+        long resultado = MULTIPLY(x, y, n);
+        
         long tempoFinal = System.nanoTime();
+        
+        System.out.println(n + " bits, x: " + x + " | y: " + y);
+        System.out.println("resultado: " + resultado);
         System.out.println("tempo gasto: " + (tempoFinal-tempoInicial));
         System.out.println("n interacoes: " + iteracoes);
+        System.out.println("---------------");
+    }
+    
+    public static void main(String[] args) {
+        
+        Ex05(15L, 15L, 4);
+        Ex05(65535L, 65535L, 16);
+        Ex05(268435455L, 268435455L, 64);
         
     }
 }
